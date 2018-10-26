ceb8fb5419ee9d6a7ff01ddc1fc0484b959384ea
HaHussain <HaHussain010@gmail.com>
2018-10-26 12:33:49 +0100

    Satisfying Linter

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
