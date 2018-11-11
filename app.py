02dfbd037856e77cc665ed06f85af69851117722
HaHussain <HaHussain010@gmail.com>
2018-11-11 21:03:31 +0000

    Fixed error in not creating redis hit counter.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Create a Flask server called app.
 cache = redis.Redis(host='redis', port=6379)
 # Create a redis cache called cache bound to port 6379.
-
+cache.set('hits', 0, nx=True)
 
 def get_hit_count():
     # When someone accesses the server, log a hit to the cache.
